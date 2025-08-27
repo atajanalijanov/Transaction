63e8043307afbbe47218226ec6b1874b50165db6
unknown <atajanalijanow5@gmail.com>
2025-08-27 15:13:22 +0500

    add:calculator balance

--- controllers/finance_calculator/actins/getIndex.js
+++ controllers/finance_calculator/actins/getIndex.js
@@ -4,7 +4,10 @@ module.exports = async (req, res) => {
     try {
         let transactions = await Transaction.findAll()
 
-        return res.render("index",{transactions});
+        const income = await Transaction.sum('amount', { where: { type: 'income' } }) || 0;
+        const expense = await Transaction.sum('amount', { where: { type: 'expense' } }) || 0;
+
+        return res.render("index",{transactions,remaining:income - expense});
     } catch(e) {
         console.log(e);
         return res.status(500).send(e.message)
