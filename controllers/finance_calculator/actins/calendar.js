650df417a27fff101b78676df8e9db4f69a3db56
unknown <atajanalijanow5@gmail.com>
2025-09-13 17:52:46 +0500

    add:monthName title

--- controllers/finance_calculator/actins/calendar.js
+++ controllers/finance_calculator/actins/calendar.js
@@ -10,12 +10,8 @@ module.exports = async (req, res) => {
             month
         } = req.params
 
-        console.log(year,month);
-
     const startDate = new Date(Date.UTC(year, month - 1, 1, 0, 0, 0));
     const endDate   = new Date(Date.UTC(year, month, 0, 23, 59, 59, 999));
-
-        console.log(startDate,endDate);
 
         let transactions = await Transaction.findAll({
             where: {
@@ -26,12 +22,23 @@ module.exports = async (req, res) => {
             },
             order: [["createdAt", "DESC"]]
         })
-        console.log(transactions,123);
 
-        const income = await Transaction.sum('amount', { where: { type: 'income', userId } }) || 0;
-        const expense = await Transaction.sum('amount', { where: { type: 'expense', userId } }) || 0;
-
-        return res.render("balance",{transactions,remaining:income - expense});
+        const income = await Transaction.sum('amount', { where: { type: 'income', userId,createdAt: {[Op.between]: [startDate, endDate]}} }) || 0;
+        const expense = await Transaction.sum('amount', { where: { type: 'expense', userId,createdAt: {[Op.between]: [startDate, endDate]} } }) || 0;
+        let monthName = '';
+        if(month == 1) { monthName = 'Yanwar'}
+        if(month == 2) { monthName = 'Fewral'}
+        if(month == 3) { monthName = 'Mart'}
+        if(month == 4) { monthName = 'Aprel'}
+        if(month == 5) { monthName = 'May'}
+        if(month == 6) { monthName = 'Iyun'}
+        if(month == 7) { monthName = 'Iyul'}
+        if(month == 8) { monthName = 'Awgust'}
+        if(month == 9) { monthName = 'Sentyabr'}
+        if(month == 10) { monthName = 'Oktyabr'}
+        if(month == 11) { monthName = 'Noyabr'}
+        if(month == 12) { monthName = 'Dekabr'}
+        return res.render("calendar",{transactions,remaining:income - expense,monthName});
     } catch(e) {
         console.log(e);
         return res.status(500).send(e.message)
